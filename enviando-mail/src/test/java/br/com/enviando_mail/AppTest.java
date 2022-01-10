93882d7e46d3566b20999d21526ac31bd0b3560e
allysoncsc <allysoncsc@gmail.com>
2022-01-10 20:52:59 -0300

    Criação do projeto

---
+++ enviando-mail/src/test/java/br/com/enviando_mail/AppTest.java
@@ -0,0 +1,17 @@
+package br.com.enviando_mail;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@org.junit.Test
+	public void testeEmail() {
+		
+		/*Olhar configurações do smtp do email*/
+	}
+}
