afd153342f1fcc4fd788e852a8389fe1acfbe6d6
allysoncsc <allysoncsc@gmail.com>
2022-01-10 21:56:20 -0300

    testando conexão

--- enviando-mail/src/test/java/br/com/enviando_mail/AppTest.java
+++ enviando-mail/src/test/java/br/com/enviando_mail/AppTest.java
@@ -1,17 +1,48 @@
 package br.com.enviando_mail;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import java.util.Properties;
+import junit.framework.Test;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 	
+	private String userName = "correiasallyson@gmail.com";
+	private String senha = "xxxxxxxxx";
+	
 	@org.junit.Test
 	public void testeEmail() {
 		
+		try {
+		
 		/*Olhar configurações do smtp do email*/
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");/*Autorização*/
+		properties.put("mail.smtp.stattls", "true");/*Autenticação*/
+		properties.put("mail.smtp.hot", "smtp.gmail.com");/*Servidor do gmail*/
+		properties.put("mail.smtp.port", "465");/*Porta do servidor*/
+		properties.put("mail.smtp.socketFactory.port", "465");/*especifica porta a ser conectada pelo socket*/
+		properties.put("mail.smtp.socketFactory.class", "javax.ssl.SSLSocketFactory");/*Classe socket da conexão ao SMTP*/
+
+		
+		Session session = Session.getInstance(properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {				
+				return new PasswordAuthentication(userName, senha);
+			}
+			
+		});
+		
+		
+		System.out.println(senha);
+		
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
